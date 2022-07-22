f914e15c2d7d1ebc5708f2e6e50dca9fa981e82d
NXuanTuong <tuongbabynhok@gmail.com>
2022-07-22 10:14:46 +0700

    TTN-13 create Header

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "../Header";
 
 type Props = {};
 
 const Layout = (props: Props) => {
   return (
     <div>
+      <Header />
       <h1 className="text-indigo-500 font-bold text-5xl text-center">
         Xin chào mọi người !
       </h1>
