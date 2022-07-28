fa6df11d2dfd382336913070736485675f4b1642
NXuanTuong <tuongbabynhok@gmail.com>
2022-07-28 21:20:03 +0700

    TTN-40 create list products

---
+++ src/hooks/use-products.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import useSWR from "swr";
+
+const useProducts = () => {
+  const { data, error, mutate } = useSWR("/products");
+  return {
+    data,
+    error,
+  };
+};
+
+export default useProducts;
