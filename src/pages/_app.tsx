76902cb7e96bf971b10833d8d8ae37fedbb24a22
NXuanTuong <tuongbabynhok@gmail.com>
2022-07-29 21:32:18 +0700

    TTN-43 install redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,17 +5,20 @@ import { SWRConfig } from "swr";
 import instance from "@/api/instance";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import { Provider } from "react-redux";
+import { store } from "@/app/store";
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const LayoutWrapper = Component.Layout ?? Layout;
   return (
-    <LayoutWrapper>
-      <SWRConfig value={{ fetcher: async (url) => await instance.get(url) }}>
-        <Component {...pageProps} />
-      </SWRConfig>
-      <ToastContainer />
-    </LayoutWrapper>
+    <Provider store={store}>
+      <LayoutWrapper>
+        <SWRConfig value={{ fetcher: async (url) => await instance.get(url) }}>
+          <Component {...pageProps} />
+        </SWRConfig>
+        <ToastContainer />
+      </LayoutWrapper>
+    </Provider>
   );
 }
 
