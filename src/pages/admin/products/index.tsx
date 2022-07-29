0ab2db328ee48ec16e42ba8bef55e89a9063f65b
NXuanTuong <tuongbabynhok@gmail.com>
2022-07-29 21:54:25 +0700

    TTN-44 update list product

--- src/pages/admin/products/index.tsx
+++ src/pages/admin/products/index.tsx
@@ -87,8 +87,8 @@ const ProductAdmin = (props: Props) => {
                       </td>
                       <td className="py-3 px-6 text-left">
                         <div className="flex items-center">
-                          <span>{item.cate}</span>
-                        </div> 
+                          <span>{item.category.name}</span>
+                        </div>
                       </td>
                       <td className="py-3 px-6 text-center">
                         <span
