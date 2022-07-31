00b8f355a2684641c4fe9f0af599a45ffa8f8d35
NXuanTuong <tuongbabynhok@gmail.com>
2022-07-31 17:34:04 +0700

    TTN-58 search product

--- src/api/product.tsx
+++ src/api/product.tsx
@@ -1,14 +1,17 @@
-import instance from "./instance"
+import instance from "./instance";
 
 export const add = (product: any) => {
-    return instance.post("/products", product)
-}
+  return instance.post("/products", product);
+};
 export const remove = (id: any) => {
-    return instance.delete(`/products/${id}`)
-}
+  return instance.delete(`/products/${id}`);
+};
 export const update = (product: any) => {
-    return instance.put(`/products/${product._id}`, product)
-}
+  return instance.put(`/products/${product._id}`, product);
+};
 export const read = (id: any) => {
-    return instance.get(`/products/${id}`)
-}
+  return instance.get(`/products/${id}`);
+};
+export const search = (q: any) => {
+  return instance.get(`/product/search?q=${q}`);
+};
